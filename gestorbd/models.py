ded28b229d84d8ee76bf170c938c14bb620bc5f8
Aleja <afajardo04@misena.edu.co>
2023-03-22 16:40:35 -0500

    modelos en base de datos

---
+++ gestorbd/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class cliente(models.Model):
+    nombre=models.CharField(max_length=30)
+    direccion=models.CharField(max_length=50)
+    email=models.EmailField(max_length=30)
+    telefono=models.CharField(max_length=20) 
+
+class articulo(models.Model):
+    nombre=models.CharField(max_length=30)
+    seccion=models.EmailField(max_length=20)
+    precio=models.IntegerField()
+
+    def __str__(self):
+        return 'Articulo: %s, Precio: %s, Seccion: %s' %(self.nombre, self.precio, self.seccion)   
+
+class pedido(models.Model):
+    numero=models.IntegerField()
+    fecha=models.DateField()
+    entregado=models.BooleanField()
--- tienda/settings.py
+++ tienda/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'clientes',
     'productos',
     "inventario",
-    "carrito_compras"
+    "carrito_compras",
+    "gestorbd"
 ]
 
 MIDDLEWARE = [
@@ -78,13 +79,25 @@ WSGI_APPLICATION = 'tienda.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "tienda",
+        "USER": "postgres",
+        "PASSWORD": "root",
+        "HOST": "127.0.0.1",
+        "DATABASE_PORT": "5432"
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
